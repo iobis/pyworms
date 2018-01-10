65eae5a8dbbaa63cb9e672ac3b5bdfe6f0bb1d56
Pieter Provoost <pieterprovoost@gmail.com>
2018-01-10 15:41:49 +0100

    init

---
+++ pyworms/__init__.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+import logging
+
+class ISODateParser(object):
+
+    def __init__(self, input):
+        self._logger = logging.getLogger(__name__)
+
+    def aphiaAttributeKeysByID(self): pass
+
+    def aphiaAttributesByAphiaID(self): pass
+
+    def aphiaAttributeValuesByCategoryID(self): pass
+
+    def aphiaIDsByAttributeKeyID(self): pass
+
+    def aphiaDistributionsByAphiaID(self): pass
+
+    def aphiaExternalIDByAphiaID(self): pass
+
+    def aphiaRecordByExternalID(self): pass
+
+    def aphiaSourcesByAphiaID(self): pass
+
+    def aphiaChildrenByAphiaID(self): pass
+
+    def aphiaClassificationByAphiaID(self): pass
+
+    def aphiaIDByName(self): pass
+
+    def aphiaNameByAphiaID(self): pass
+
+    def aphiaRecordByAphiaID(self): pass
+
+    def aphiaRecordsByDate(self): pass
+
+    def aphiaRecordsByMatchNames(self): pass
+
+    def aphiaRecordsByName(self): pass
+
+    def aphiaRecordsByNames(self): pass
+
+    def aphiaSynonymsByAphiaID(self): pass
+
+    def aphiaRecordsByVernacular(self): pass
+
+    def aphiaVernacularsByAphiaID(self): pass
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="pyworms",
+    description="Python client for the WoRMS REST service",
+    author="Pieter Provoost",
+    author_email="pieterprovoost@gmail.com",
+    version="0.1.0",
+    packages=["pyworms"]
+)
