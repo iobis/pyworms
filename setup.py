e8864ec128404d6ff6f43e2d117f244b4ce9dc06
Pieter Provoost <pieterprovoost@gmail.com>
2023-01-19 12:23:25 +0100

    setup fix

--- setup.py
+++ setup.py
@@ -7,10 +7,6 @@ setup(
     author_email="pieterprovoost@gmail.com",
     version="0.3.0",
     packages=["pyworms"],
-<<<<<<< HEAD
-    install_requires=["backports.functools_lru_cache", "requests"]
-=======
     install_requires=["backports.functools_lru_cache", "requests"],
     license="BSD",
->>>>>>> 7144b43670cec330341abd5d3c42ca63996f1c78
 )
